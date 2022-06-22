6d87d0e3063b50ed69e0d6eee549637686e48832
wmonsalv <55903739+wmonsalv@users.noreply.github.com>
2022-06-21 21:35:40 -0400

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,8 +34,10 @@ class Home extends Component {
     // dealing with a larger data sensitive project.
     todo.id = Math.random();
     // Create a array that contains the current array and the new todo item
+    
     let new_list = [...this.state.todos, todo];
     // Update the local state with the new array.
+
     this.setState({
       todos: new_list,
     });
